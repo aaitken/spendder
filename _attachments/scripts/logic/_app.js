f53bec2b9549705eed047d4ad8f8aa6b092b0e51
Alex Aitken <aaitken@alexaitken.net>
2011-07-30 17:09:09 -0600

    _app.js refactor; session management work

--- _attachments/scripts/logic/_app.js
+++ _attachments/scripts/logic/_app.js
@@ -7,7 +7,7 @@
 
 	//PROPERTIES========================================================================================================
 	app.host='http://127.0.0.1:5984/';
-	app.pathShow='spendder/_design/spendder/_show/';
+	app.path='spendder/_design/spendder/';
 	app.waitOnRequirements=false; //for require dependencies within page-specific files
 	app.jsCache={};
 
@@ -24,57 +24,84 @@
 		//showReceipt
 		this.subscribe(this.renderShow,'showReceipt');
 		this.subscribe(this.updateHistory,'showReceipt');
-		//showRequest
-		this.subscribe(this.hitShow,'showRequest');
+		//urlRequest
+		this.subscribe(this.hitUrl,'urlRequest');
 	};
 
 	//METHODS===========================================================================================================
 
-	//request couchdb show
-	app.hitShow=function(obj){
+	/*
+	* hit a couchdb api url
+	* obj keys: url, mthd, history
+	*/
+	app.hitUrl=function(obj){
 
-		var namespace=obj.show.split('.')[0], //root part of show name, minus the .html
-			requireArray=['text!/spendder/_design/spendder/_show/'+obj.show],
-			callback=null; //page init function to fire depending on if code has already been loaded
+		//private functions for hitting the couchdb apis of same name
+		var show,
+			session;
 
-		if(!SPNDR.page[namespace]){ //if we haven't already loaded this file
-			requireArray=requireArray.concat(['scripts/logic/'+namespace+'.js']);
-			callback='init';
-		}
-		else{
-			callback='setup';
-		}
+		show=function(obj){
 
-		require(requireArray,function(html){
+			var namespace=obj.url.split('.')[0], //root part of show name, minus the .html
+				requireArray=['text!/spendder/_design/spendder/_show/'+obj.url],
+				callback=null; //page init function to fire depending on if code has already been loaded
 
-			var publish=function(){
-					that.publish({
-						show:obj.show,
-						history:obj.history,
-						response:html,
-						callback:SPNDR.page[namespace][callback]
-					},'showReceipt'); //--------------------------------------------------------------------------->
-				},
-				poll; //polling interval for requires within page-specific files
-
-			//Branching below accounts for cases in which supporting JS file itself includes a require call - in these
-			//cases nested require is set to true on file download, and false again at bottom of require's callback
-			if(!app.waitOnRequirements){publish()}
+			if(!SPNDR.page[namespace]){ //if we haven't already loaded this file
+				requireArray.push('scripts/logic/'+namespace+'.js');
+				callback='init';
+			}
 			else{
-				poll=setInterval(function(){
-					if(!app.waitOnRequirements){
-						publish();
-						clearInterval(poll);
-					}
-				},100);
+				callback='setup';
 			}
 
-		});
+			require(requireArray,function(html){
+
+				var publish=function(){
+						that.publish({
+							url:obj.url,
+							history:obj.history,
+							response:html,
+							callback:SPNDR.page[namespace][callback]
+						},'showReceipt'); //----------------------------------------------------------------------->
+					},
+					poll; //polling interval for requires within page-specific files
+
+				//Branching below accounts for cases in which supporting JS file itself includes a require call - in these
+				//cases nested require is set to true on file download, and false again at bottom of require's callback
+				if(!app.waitOnRequirements){publish()}
+				else{
+					poll=setInterval(function(){
+						if(!app.waitOnRequirements){
+							publish();
+							clearInterval(poll);
+						}
+					},100);
+				}
+
+			});
+		};
+		session=function(obj){
+			$.ajax({
+				type:obj.mthd,
+				url:that.host+'_session',
+				success:function(){
+					alert('logged out');
+				},
+				error:function(xhr,error){}
+			});
+		};
+
+		switch(obj.api){
+			case 'show':show(obj);
+			case 'session':session(obj);
+			default: return;
+		}
+
 	}.bind(app);
 
 	//browser url and history management
 	app.updateHistory=function(obj){
-		if(obj.history){window.history.pushState(null,'',obj.show)}
+		if(obj.history){window.history.pushState(null,'',obj.url)}
 	};
 
 	//display the couchdb's response to our show request - todo: combine with hitShow if nothing else depends on response
@@ -86,24 +113,25 @@
 	//setup
 	app.setup=function(){
 
-		//listener setup for clicks on els with data-show, publish the attribute's value
-		$('a[data-show]').live('click',function(e){
+		//listener setup for clicks on els with data-url, publish the attribute's value
+		$('a[data-api]').live('click',function(e){
+			var $targ=$(e.target);
 			that.publish({
-				show:$(e.target).attr('data-show'),
+				url:$targ.attr('data-url')||null, //session api, for example, only has a single url
+				api:$targ.attr('data-api'),
+				mthd:$targ.attr('data-mthd')||null, //will default to 'GET' in api
 				history:true
-			},'showRequest'); //----------------------------------------------------------------------------------->
+			},'urlRequest'); //----------------------------------------------------------------------------------->
 		});
 
 		//history management - init param = function to fire on popstate
 		SPNDR.scaffolding.history.init(function(){
 			that.publish({
-				show:window.location.href.split('/').pop(),
+				url:window.location.href.split('/').pop(),
+				api:'show',
 				history:false
-			},'showRequest'); //----------------------------------------------------------------------------------->
+			},'urlRequest'); //----------------------------------------------------------------------------------->
 		});
-
-		//
-		#('logout').bind('click')
 
 	}.bind(app);
 
